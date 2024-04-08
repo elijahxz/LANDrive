eb86089d1896ea9807fe3669f29a19b58b04d904
elijahxz <elijahatkins911@yahoo.com>
2024-04-07 21:45:08 -0500

    Updated UI using QT Developer, themes are now changable and everything works like yesterday

--- client/client.py
+++ client/client.py
@@ -1,141 +1,80 @@
+import json
 import time
 import socket
 import sys
-from PyQt6.QtCore import QSize, Qt
-from PyQt6.QtGui import QAction
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, \
-                            QLabel, QLineEdit, QVBoxLayout, QWidget, \
-                            QMenu, QStackedWidget, QHBoxLayout
 
-""" 
-    Most of the PyQt6 documentation I found from a tutorial here:
-        https://www.pythonguis.com/pyqt6-tutorial/
+# Whew, thats alot.
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QAbstractItemView, QAbstractScrollArea, QApplication, QFrame,
+    QHBoxLayout, QHeaderView, QLabel, QLayout,
+    QMainWindow, QPushButton, QSizePolicy, QStackedWidget,
+    QTableWidget, QTableWidgetItem, QTextEdit, QVBoxLayout,
+    QWidget, QMenu, QStyleFactory)
+
+"""  
+    Ui_Mainwindow is the barebones GUI I created in QT Developer
+    I tried making one using my own code, but take a look in UserInterface.py and 
+    you'll see why I used the tool instead :) ... It's a nightmare to program. 
 """
+from UserInterface import Ui_MainWindow
 
-# Notes:
-#super().mousePressEvent(event)
-# e.accept() marks an event as handled
-# e.ignore() marks an event as unhandled
-# css styling
-# connect_button.setObjectName("connect")
-    
-
-# This app does not ask the client to insert a port, 
+# This app does not ask the client to insert a port,
 # so the server and client always tries to access
 # 8504 on localhost
 HOST = "127.0.0.1"
 PORT = 8504
+
+MAX_SIZE = 16384
+
 CLOSE_CONNECTION = "TerminateTCPConnection"
+REFRESH = "RefreshFiles"
 
-"""
-    I personally think this style of coding is ugly, but this sets up the window with 
-    the connect screen, and the main screen.
-"""
-class MainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
+# QT Developer chose these. To change the screen, use we
+# self.ui.stackedWidget.setCurrentIndex(3)
+MAIN_SCREEN = 1
+EDIT_SCREEN = 2
+CONNECT_SCREEN = 0
 
-        # Create a socket for the client  
-        self.c_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+""" 
+   The window is a wrapper class for Ui_MainWindow so we can interact with it. 
+"""
+class Window(QMainWindow):
+    def __init__(self, parent=None):
+        super(Window, self).__init__(parent)
+        # Used for connecting to the server 
         self.CONNECTING = False
 
-        """
-            Start of GUI initialization
-        """
-        # Window Setup 
-        self.setWindowTitle("LANDrive")
-        self.setMinimumSize(1024,768)
+        self.THEMES = QStyleFactory.keys()
+        self.current_theme = app.style().name()
+
+        # Create a socket for the client
+        self.c_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+        # Initialize the other class and set everything up
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.setupUiFunctionality()
 
 
         # Set exit program option on the context menu
         self.context_menu = QMenu(self)
-        action1 = self.context_menu.addAction("Exit Program")
-        action1.triggered.connect(QApplication.quit)
-      
-
-        # Connect screen setup
-        self.connect_widget = QWidget()
+        action1 = self.context_menu.addAction("Change Theme")
+        action1.triggered.connect(self.changeTheme)
+        action2 = self.context_menu.addAction("Exit Program")
+        action2.triggered.connect(QApplication.quit)
+   
+    def setupUiFunctionality(self):
+        self.ui.connect_button.clicked.connect(self.connectToServer)
+        self.ui.disconnect_button.clicked.connect(self.disconnectFromServer)
         
-        # Allows class to see this because multiple functions access it
-        self.connect_label = QLabel()
-        
-        self.setupConnectScreen()
-
-
-        # Main screen setup
-        self.main_widget = QWidget()
-        
-        self.setupMainScreen()
-
-
-        # Container Setup
-        self.container = QStackedWidget()
-        self.container.addWidget(self.connect_widget)
-        self.container.addWidget(self.main_widget) 
-
-        self.setCentralWidget(self.container)
-        
-        self.container.setCurrentWidget(self.connect_widget)
-
-        """
-            End of GUI initialization
-        """
-    
-    """ 
-        Two screens cant share elements, so we have to create 
-        Multiple objects, even if they share the same properties
-        PyQt also allows series classes, but this works fine.
-    """
-    def createHeader(self, name):
-        header = QLabel(name)
-        
-        font = header.font()
-        font.setPointSize(50)
-        header.setFont(font)
-        
-        header.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignTop)
-        
-        return header
-
-    def setupConnectScreen(self):
-        # Connect Button
-        connect_button = QPushButton(text="Connect", parent=self)
-        connect_layout = QVBoxLayout()
-        
-        connect_button.clicked.connect(self.connectToServer)
-        connect_button.setObjectName("connect") 
-        connect_button.setFixedSize(500, 200)
-    
-        button_font = connect_button.font()
-        button_font.setPointSize(35)
-        connect_button.setFont(button_font)
-        
-        # Layout Setup
-        connect_layout.addWidget(self.createHeader("LANDrive"))
-        connect_layout.addWidget(connect_button)
-        connect_layout.addWidget(self.connect_label)
-        connect_layout.setAlignment(Qt.AlignmentFlag.AlignHCenter) 
-       
-        # Add layout to the widget
-        self.connect_widget.setLayout(connect_layout)
-       
-        return
-
-    def setupMainScreen(self):
-        main_layout = QVBoxLayout()
-        main_top_layout = QHBoxLayout()
-        main_middle_layout = QHBoxLayout()
-        main_bottom_layout = QHBoxLayout()
-        
-        main_top_layout.addWidget(self.createHeader("LANDrive"))
-
-        main_layout.addLayout(main_top_layout)
-        main_layout.addLayout(main_middle_layout)
-        main_layout.addLayout(main_bottom_layout)
-        
-        self.main_widget.setLayout(main_layout)
-
-        return
 
     """ Called when the GUI is closed """
     def closeEvent(self, event):
@@ -146,59 +85,102 @@ class MainWindow(QMainWindow):
             self.c_socket.close()
         except Exception:
             pass
-
+    
     """ Used for right clicking on the mouse """
     def contextMenuEvent(self, e):
-        self.context_menu.exec(e.globalPos()) 
-   
-    """ 
-        Connect button event, trys to connect to the server 
+        self.context_menu.exec(e.globalPos())
+
+    
+    # Changes the theme of the app (called by a menuevent)
+    def changeTheme(self):
+        counter = 0
+        length = len(self.THEMES) - 1
+        
+        if (length < 0):
+            return
+        
+        for theme in self.THEMES: 
+            if self.current_theme == theme:
+                break
+            counter += 1
+        
+        counter += 1
+        if counter > length: 
+            counter = 0
+
+        self.current_theme = self.THEMES[counter] 
+        app.setStyle(self.current_theme)
+        return
+
+    """
+        Connect button event, trys to connect to the server
         NOTE: The time.sleep() functions just make the UI look
-              a little better ... since we are connecting to 
+              a little better ... since we are connecting to
               localhost its super fast
     """
     def connectToServer(self):
-        
+
         # We cant allow the user to spam the connect button,
         # or else the program will crash :)
         if self.CONNECTING:
             return
 
         # Force non-async update for the connecting... message
-        self.connect_label.setText("Connecting...")
-        self.connect_label.repaint()
+        self.ui.connect_label.setText("Connecting...")
+        self.ui.connect_label.repaint()
         time.sleep(1)
 
         try:
             self.CONNECTING = True
-            
+
             # Pass in a tuple to connect()
             self.c_socket.connect((HOST, PORT))
         except ConnectionRefusedError:
-            self.connect_label.setText("Error: Connection to server refused. " + 
+            self.ui.connect_label.setText("Error: Connection to server refused. " +
                                "Please try again or check the server status")
             self.CONNECTING = False
             return
         self.CONNECTING = False
-        self.connect_label.setText("Success! Connected to the server!")
-        self.connect_label.repaint()
-        
+        self.ui.connect_label.setText("Success! Connected to the server!")
+        self.ui.connect_label.repaint()
+
         time.sleep(2)
 
         self.displayMainScreen()
+
+    def disconnectFromServer(self):
+        self.c_socket.send(CLOSE_CONNECTION.encode())
+        self.c_socket.close()
+
+        self.c_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+        self.displayConnectScreen()
     
     def displayMainScreen(self):
         # Hide connection and show the main widget
-        self.connect_label.setText("")
-        self.container.setCurrentWidget(self.main_widget)
+        self.ui.connect_label.setText("")
+        self.ui.stackedWidget.setCurrentIndex(MAIN_SCREEN)
+
+    def displayConnectScreen(self):
+        self.ui.stackedWidget.setCurrentIndex(CONNECT_SCREEN)
+
+    def fetchServerFiles(self):
+        self.c_socket.send("RefreshFiles".encode())
+
+        data = self.c_socket.recv(MAX_SIZE)
+        data = data.decode("utf-8")
+
+        files = json.loads(data)
+
+        return files
 
 
-def main():
-    # Display the GUI to the user
-    app = QApplication([])
-    window = MainWindow()
-    window.show()
 
-    app.exec()
 
-main()
+
+
+app = QApplication([])
+window = Window()
+window.show()
+
+app.exec()
--- server/server.py
+++ server/server.py
@@ -1,7 +1,11 @@
+import json
 import os
 import socket
 import sys
+import time
 import threading
+
+from enum import StrEnum
 
 # This app does not ask the client to insert a port,
 # so the server and client always tries to access
@@ -11,34 +15,29 @@ PORT = 8504
 MAX_USERS = 16
 # Random max size, may increase if needed
 MAX_SIZE = 16384
-CLOSE_CONNECTION = "TerminateTCPConnection"
 PRINT_LOCK = threading.Lock()
 
-# Function to handle client connections
-def client_handler(c_socket):
-    
-    while True:
-        # Receive data from the client
-        request = c_socket.recv(MAX_SIZE)
-        
-        buffer = request.decode()
-        
-        stdoutPrint("Client message: %s" % (buffer))
-        if (buffer == CLOSE_CONNECTION):
-            PRINT_LOCK.release()
-            break
+SERVER_ROOT_DIR = "\\FileDirectory"
 
-        # Send a response back to the client
-        response = "Hello from the server!"
-        c_socket.send(response.encode())
-        
-    # Close the connection with the client
-    c_socket.close()
+SERVER_FILES = list()
+
+SERVER_TERMINATE = False
+
+
+""" 
+    Commands that can be requested from the client
+    They are in a module because match/case statements
+    semantics are not the same if variables are used
+"""
+class ResponseCode(StrEnum):
+    CLOSE_CONNECTION = "TerminateTCPConnection"
+    REFRESH          = "RefreshFiles"
 
 
 def main():
-    stdoutPrint("Server running...\n")
+    global SERVER_TERMINATE
 
+    stdoutPrint("Server running...\n")
     # TCP socket
     s_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -48,7 +47,10 @@ def main():
     # Allow up to 16 users at once
     s_socket.listen(MAX_USERS)
     
-    stdoutPrint("Server now listening on %s: %d\n" % (HOST, PORT))
+    stdoutPrint("Server now listening on %s:%d\n" % (HOST, PORT))
+   
+    file_thread = threading.Thread(target=getServerFiles)
+    file_thread.start()
 
     try:
         while True:
@@ -65,6 +67,10 @@ def main():
     # When Ctrl + C, show that the server is closing
     except KeyboardInterrupt:
         stdoutPrint("Server shutting down...")
+        
+        SERVER_TERMINATE = True
+        file_thread.join()
+
         s_socket.close()
         sys.exit()
     
@@ -76,7 +82,72 @@ def stdoutPrint(message):
     print(message)
     sys.stdout.flush()
 
+# Function to handle client connections
+def client_handler(c_socket):
+    files = list()
+
+    while True:
+        # Receive data from the client
+        request = c_socket.recv(MAX_SIZE)
+        
+        buffer = request.decode()
+        
+        match buffer:
+            case ResponseCode.CLOSE_CONNECTION:
+                PRINT_LOCK.release()
+                break # This breaks the while loop!
+            # Send the files in the server's directory to the user
+            case ResponseCode.REFRESH:
+                data = json.dumps(SERVER_FILES)
+                c_socket.sendall(bytes(data, encoding="utf-8"))
+            case _:
+                stdoutPrint("An error has occured, %s is not a valid client request" 
+                            % (buffer))
+         
 
 
+
+        stdoutPrint("Client message: %s" % (buffer))
+        if (buffer == ResponseCode.CLOSE_CONNECTION):
+            PRINT_LOCK.release()
+            break
+
+        # Send a response back to the client
+        response = "Hello from the server!"
+        c_socket.send(response.encode())
+        
+    # Close the connection with the client
+    c_socket.close()
+
+"""
+    Get an updated list of available files from the server
+    Output structure:
+        A list of lists, each sublist looks like this:
+            [relative path, file name]
+"""
+def getServerFiles():
+    global SERVER_FILES
+    file_list = list()
+    
+    while not SERVER_TERMINATE: 
+        # Gets the path of this file, server.py. 
+        # FileDirectory/ should be at the same path 
+        server_py_path = os.path.dirname(os.path.realpath(__file__)) 
+        file_directory_path = server_py_path + SERVER_ROOT_DIR
+        
+        # Get all of the files unser FileDirectory\
+        for path, subdirs, files in os.walk(file_directory_path):
+            rel_path = path[path.find(SERVER_ROOT_DIR):]
+            current_dir = os.path.basename(rel_path)
+            for name in files:
+                file_list.append([os.path.join(rel_path, name),current_dir,name])
+        
+        SERVER_FILES = file_list
+   
+        # The corresponding thread will check and update the files 
+        # Approx. every 10 seconds
+        time.sleep(10)
+
+# Start program by calling main()
 if __name__ == "__main__":
     main()
